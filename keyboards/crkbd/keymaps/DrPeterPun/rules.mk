2445933ef261bca2c9819adc2054a923f4a4342f
DrPeterPun <61242967+DrPeterPun@users.noreply.github.com>
2023-03-05 19:19:56 +0000

    Update rules.mk

--- keyboards/crkbd/keymaps/DrPeterPun/rules.mk
+++ keyboards/crkbd/keymaps/DrPeterPun/rules.mk
@@ -2,4 +2,4 @@ MOUSEKEY_ENABLE = no     # Mouse keys
 RGBLIGHT_ENABLE = no     # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
-LTO_ENABLE      = yes
+#LTO_ENABLE      = yes
